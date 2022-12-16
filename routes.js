90169aa2cda4571394cdd984347fc82bb44a2b1c
Kryzztian <cdkittsteiner@yahoo.com>
2022-12-16 14:28:29 -0600

    changes so render would work

--- routes.js
+++ routes.js
@@ -1,10 +1,7 @@
 import express from "express";
 import postgres from "postgres"; 
 
-let sql = postgres({
-    database: 'ffxiv',
-    password: "0"
-});
+let sql = postgres("postgres://ffxiv_collectables_user:NmvvR0ExfRM7CT0Bz2aFwa4RthUrL20j@dpg-ceecv29gp3jlc78tipe0-a.oregon-postgres.render.com/ffxiv_collectables?ssl=true");
 //'?ssl=true'
 let app = express();
 app.use(express.json());
