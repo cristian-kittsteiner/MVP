9d5d8170c937e864d85afa4c9cd0f68016568ddb
Kryzztian <cdkittsteiner@yahoo.com>
2022-12-16 15:41:18 -0600

    testing app.js route

--- public/app.js
+++ public/app.js
@@ -1,5 +1,30 @@
-fetch("http://localhost:3000/minions")
+//const axios = require('axios').default;
+//import axios from "axios";
+//require not defined and there is no package.json file to dfine type:module so import wont work
+
+// import axios from "axios";
+
+
+fetch("https://ffxiv-collectables.onrender.com/minions")
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
     });
+
+// axios({
+//     method: 'get',
+//     url: '/minions',
+//     baseURL: 'http://localhost:3000/'
+// }).then((response)=>{
+//     console.log(response.data)
+// });
+
+// axios({
+//     method: 'get',
+//     url: '/minions',
+//     baseURL: 'https://ffxiv-collectables.onrender.com/'
+// }).then((response)=>{
+//     console.log(response.data)
+// })
+//can use object key (transfromRequest) to transform the request data
+// similar with (transformResponse) but for res data
